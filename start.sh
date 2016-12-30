231663574fe2242b1d7ae5a7240e9271392280be
Hai Ren <myrenhai@gmail.com>
2016-12-29 18:16:26 -0800

    modify start script

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 cd /root/workspace/virgo/ && \
 git pull origin master && \
-nohup /root/workspace/virgo/venv/bin/python2.7 /root/workspace/virgo/venv/bin/scrapy crawl rottentomatoes \
+nohup /root/workspace/virgo/venv/bin/python2.7 /root/workspace/virgo/venv/bin/scrapy crawl CelebrityWithMovie \
 -s JOBDIR=/root/data/scrapy_job \
 --logfile=/var/log/rottentomatoes.log --loglevel=INFO >./nohup.log 2>&1 & \
